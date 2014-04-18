1d9b552de5ee1f6318b9c3e0d2cec79603f1897a
hh <hhhh>
2014-04-18 18:14:18 -0300

    fix top 5

--- layout/widgets/topplayers.php
+++ layout/widgets/topplayers.php
@@ -3,10 +3,10 @@
 	<?php
 
 	$highscore = $config['highscore'];
-	
+	$g = $highscore['ignoreGroupId'];
 	$cache = new Cache('engine/cache/topPlayer');
 	if ($cache->hasExpired()) {
-		$players = mysql_select_multi("SELECT `name`, `level`, `experience` FROM `players`  WHERE `group_id` < $highscore['ignoreGroupId'] ORDER BY `experience` DESC LIMIT 5;");
+		$players = mysql_select_multi("SELECT `name`, `level`, `experience` FROM `players`  WHERE `group_id` < $g ORDER BY `experience` DESC LIMIT 5;");
 		
 		$cache->setContent($players);
 		$cache->save();
