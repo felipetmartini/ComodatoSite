72619b91c0cd408f6a880a4d26510dab2fd4a037
felipetmartini <felipemartini@brturbo.com.br>
2014-05-26 16:47:14 -0300

    add pagueseguro

--- config.php
+++ config.php
@@ -638,6 +638,26 @@
 		30 => 420, // +40% bonus
 	);
 
+
+	/*
+	Retorno PagSeguro 2.0 - PHP e MySQL
+	por Diogo Dourado - www.dourado.net
+	Última Atualização: 09/06/2011
+ 
+	Se você ainda não é cadastrado no PagSeguro, utilize o link abaixo para se cadastrar:
+	https://pagseguro.uol.com.br/?ind=528005
+	*/
+	$config['pagueseguro'] = array(
+		'enabled' => true,
+		'retorno_site' => "http://".$_SERVER['HTTP_HOST']."/success.php",  // Site para onde o usuário vai ser redirecionado ao termino do pagamento
+		'retorno_token' => 'PREENCHA'; // Token gerado pelo PagSeguro
+		'retorno_host' => 'localhost'; // Local da base de dados MySql
+		'retorno_database' => 'PREENCHA'; // Nome da base de dados MySql
+		'retorno_usuario' => 'PREENCHA'; // Usuario com acesso a base de dados MySql
+		'retorno_senha' => 'PREENCHA';  // Senha de acesso a base de dados MySql
+	);
+	
+	
 	//////////////////
 	/// PAYGOL SMS ///
 	//////////////////
