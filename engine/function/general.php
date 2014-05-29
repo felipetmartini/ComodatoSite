9169ff262d5099fd2a6db8a4751c707dd2f4bd09
felipetmartini <felipemartini@brturbo.com.br>
2014-05-29 20:13:37 -0300

    ..

--- engine/function/general.php
+++ engine/function/general.php
@@ -91,7 +91,7 @@ function calculate_discount($orig, $new) {
 // Proper URLs
 function url($path = false) {
 	$protocol = 'http://worldofcomodato.com';
-	$domain   = 'worldofcomodato.com/' . ($_SERVER['SERVER_PORT'] != 80 ? ':' . $_SERVER['SERVER_PORT'] : null);
+	$domain   = 'worldofcomodato.com' . ($_SERVER['SERVER_PORT'] != 80 ? ':' . $_SERVER['SERVER_PORT'] : null);
 	$folder   = dirname($_SERVER['SCRIPT_NAME']);
 
 	return $protocol . $domain . ($folder == '/' ? '' : $folder) . '/' . $path;
