95d8743948a17aa6827fb65eef4cd4c6fe860597
felipetmartini <felipemartini@brturbo.com.br>
2014-05-29 00:11:17 -0300

    ..

--- serverinfo.php
+++ serverinfo.php
@@ -30,6 +30,13 @@ if (file_exists($config['server_path'].'/data/XML/stages.xml') && file_exists($c
 
 		echo '</tbody></table>';
 
+		echo "<h2>Server rates</h2>
+		<table class='table table-striped table-hover'>
+		<tbody><tr class='yellow'><td>Skills rate</td><td>Magic rate</td><td>Loot rate</td></tr>
+		<tr><td><center>x".$lua_path['rateSkill']."</center></td><td><center>x".$lua_path['rateMagic']."</center></td><td><center>x".$lua_path['rateLoot']."</center></td></tr>
+		</tbody></table>";
+	}
+		
 	} else {
 
 		// Not using stages
